891c97fb439e52734528c75358781c57ea333225
lilly-y-k <yuriko.0203.kawabata@gmail.com>
2025-04-08 00:04:27 +0900

    TEST

--- vue3studylist/src/views/List3-2.vue
+++ vue3studylist/src/views/List3-2.vue
@@ -1,5 +1,24 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import VCodeBlock from '@wdns/vue-code-block'
+import { ref } from 'vue'
+
+const code3_2 = ref(`
+  import { ref } from "vue"
+  const name = ref("田中太郎");
+`)
+
+
+
+</script>
 
 <template>
-  <p>例１</p>
+  <div>
+    <h1></h1>
+    <VCodeBlock :code="code3_2" highlightjs label="リスト3-2" lang="javascript" theme="neon-bunny" />
+    
+  
+  
+  
+  </div>
+
 </template>
