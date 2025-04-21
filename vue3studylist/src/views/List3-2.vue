fba9437f2ef88cc4cb2520574c96603bf575cfe1
lilly-y-k <yuriko.0203.kawabata@gmail.com>
2025-04-21 19:31:32 +0900

    changeFileName

--- vue3studylist/src/views/List3-2.vue
+++ vue3studylist/src/views/List3-2.vue
@@ -1,20 +1,50 @@
 <script setup lang="ts">
 import VCodeBlock from '@wdns/vue-code-block'
-import { ref } from 'vue'
 
-const code3_2 = ref(`
-  import { ref } from "vue"
-  const name = ref("田中太郎");
+// sample
+import { ref } from 'vue'
+const name = ref('田中太郎')
+
+// typescriptの内容（codeBlocke用）
+const codeScript = ref(`import { ref } from 'vue'
+
+const name = ref('田中太郎')
 `)
 
-
-
+// htmlの内容（codeBlocke用）
+const codeTemplate = ref(`<h1>こんにちは！{{ name }}さん！</h1>
+`)
 </script>
 
 <template>
-  <div>
-    <h1></h1>
-      <VCodeBlock :code="code3_2" highlightjs label="リスト3-2" lang="javascript" theme="neon-bunny" />
+  <h1>リスト3-2</h1>
+  <VCodeBlock :code="codeScript" highlightjs label="App.vue-script" lang="javascript" theme="neon-bunny" />
+  <VCodeBlock :code="codeTemplate" highlightjs label="App.vue-template" lang="html" theme="neon-bunny" />
+
+  <br>
+
+  <p>表示例</p>
+
+  <div class="retro-game-window">
+    <!-- sample -->
+    <h1>こんにちは！{{ name }}さん！</h1>
   </div>
 
+  <p>ポイント</p>
+  <li v-pre>マスタッシュ構文：{{  }}</li>
+  <li>テンプレート変数：ref()</li>
 </template>
+
+<style lang="css">
+.retro-game-window{
+  background-color: #00000033;
+  box-shadow: 0 0 0 4px #000000 inset, 0 0 0 8px #ffffff inset;
+  box-sizing: border-box;
+  padding: 16px;
+  margin: 16px 0; /* 上下の余白のみ設定し、左端に寄せる */
+
+  width: 100%; /* 画面サイズいっぱいに */
+  min-height: 96px; /* 最小の高さを指定 */
+  color: #000000;
+}
+</style>
